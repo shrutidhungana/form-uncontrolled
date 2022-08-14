ce6d0102144c6bb7c4531c5dd93cc447736651ba
shrutidhungana <shrutidhungana@prixa.net>
2022-08-14 16:10:34 +0545

    Managing form using useRef

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { createRef, useState } from "react";
 
 const Form = () => {
 
-  const handleFormSubmit = () =>{}
+    const nameRef = createRef(null);
+    const ageRef = createRef(null);
+    const [formData , setFormData] = useState({})
+    const handleFormSubmit = (e) => {
+      e.preventDefault()
+  };
 
   return (
-      <div>
-          <form action = "" onSubmit = {handleFormSubmit}>
-              <input name = "name" placeholder = "Name"/>
-              <input />
-              <button>Submit</button>
-          </form>
-      </div>
-  )
-}
+    <div>
+      <form action="" onSubmit={handleFormSubmit}>
+        <input name="name" type="text" placeholder="Name" ref={nameRef} />
+        <input name="age" type="number" placeholder="Age" ref={ageRef} />
+        <button type = "submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
-export default Form
+export default Form;
