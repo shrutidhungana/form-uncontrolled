9e36bedd107016becc1860f05a2d831b070037c7
shrutidhungana <shrutidhungana@prixa.net>
2022-08-14 16:47:12 +0545

    Completed form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Form from './Form/Form'
+import './App.css'
 
 const App = () => {
   return (
-    <Form />
+    <div className = "App">
+      <Form />
+      </div>
   )
 }
 
--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,21 +1,36 @@
 import React, { createRef, useState } from "react";
 
 const Form = () => {
+  const nameRef = createRef(null);
+  const ageRef = createRef(null);
+  const [formData, setFormData] = useState({});
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const extractCurrentValueFromName = nameRef.current.value;
+    const extractCurrentValueFromAge = ageRef.current.value;
+    console.log(extractCurrentValueFromName, extractCurrentValueFromAge);
 
-    const nameRef = createRef(null);
-    const ageRef = createRef(null);
-    const [formData , setFormData] = useState({})
-    const handleFormSubmit = (e) => {
-      e.preventDefault()
+    setFormData({
+      name: extractCurrentValueFromName,
+      age: extractCurrentValueFromAge,
+    });
+
+    nameRef.current.value = "";
+    ageRef.current.value = "";
   };
-
   return (
     <div>
       <form action="" onSubmit={handleFormSubmit}>
-        <input name="name" type="text" placeholder="Name" ref={nameRef} />
-        <input name="age" type="number" placeholder="Age" ref={ageRef} />
-        <button type = "submit">Submit</button>
+        <input type="text" name="name" placeholder="Name" ref={nameRef} />
+        <input type="number" name="age" placeholder="Age" ref={ageRef} />
+        <button type="submit">Submit</button>
       </form>
+      {formData && Object.keys(formData).length > 0 ? (
+        <div>
+          <p>The name is {formData.name}</p>
+          <p>Age is {formData.age}</p>
+        </div>
+      ) : null}
     </div>
   );
 };
